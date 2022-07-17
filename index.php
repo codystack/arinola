18d1f969a8b7c91ee5606d0e5166809bd1bbe5cd
ThankGod Okoro <thankgodokoro@ThankGods-MBP.lan>
2022-07-17 09:09:16 +0100

    index page added

---
+++ index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Arinola @ 30&trade;</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="ThankGod Okoro">
+	<meta name="description" content="Arinols 30th Birthday party invite">
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900%7CPlayfair+Display:400,400i,700,700i%7CRoboto:400,400i,500,700" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="assets/vendor/font-awesome/css/all.min.css" />
+	<link rel="stylesheet" type="text/css" href="assets/vendor/themify-icons/css/themify-icons.css" />
+	<link rel="stylesheet" type="text/css" href="assets/vendor/animate/animate.min.css" />
+	<link rel="stylesheet" type="text/css" href="assets/vendor/glightbox/css/glightbox.css">
+	<link rel="stylesheet" type="text/css" href="assets/vendor/tiny-slider/tiny-slider.css">
+	<link rel="stylesheet" type="text/css" href="assets/vendor/aos/aos.css">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+
+</head>
+
+<body>
+	
+	<section class="p-0 d-flex align-items-center personal-portfolio">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-12 col-md-6 col-lg-6 personal-portfolio-profile vh-100" style="background:url(assets/images/arin.jpg) no-repeat; background-repeat: no-repeat; background-position: center top; background-size: cover; background-attachment: scroll; z-index: 0;">
+					<div class="p-3 p-xl-5 p-xs-10 all-text-white portfolio-info">
+						<h2 class="alt-font display-4 fst-italic fw-bold">Arinola @ 30</h2>
+						<p class="">Please join us for an intimate evening of music, cocktails and dinner as Arin turns 30.</p>
+                        <div class="btn btn-grad zoom-on-hover">
+							<a data-glightbox="" href="https://forms.gle/32QtRaomBRv2sWkEA"> <i class="fa fa-plus-circle text-white"></i> Reserve a Seat</a>
+						</div>
+					</div>
+				</div>
+				<!-- Right -->
+				<div class="col-12 col-md-6 col-lg-6 personal-portfolio-main offset-md-6 offset-lg-6 position-relative">
+					<div class="row mx-auto">
+
+						<!-- page title Start -->
+						<div class="col-ms-12">
+							<h2 class="display-5 mt-5 fw-bold">Venue</h2>
+							<img src="assets/images/venue.jpg" alt="venue">
+						</div>
+
+                        <div class="col-md-12">
+                            <div class="h-100">
+                                <iframe class="w-100 h-100 grayscale rounded" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.614981727957!2d3.4183102253113082!3d6.443457810984587!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bf4d46fa3d73f%3A0x984185e720df5423!2s1%20Grillo%20St%2C%20Ikoyi%20106104%2C%20Lagos%2C%20Nigeria!5e0!3m2!1sen!2stg!4v1658044609360!5m2!1sen!2stg" style="border:0;" aria-hidden="false" tabindex="0"></iframe>	
+                            </div>
+                        </div>
+                        
+
+						<footer class="col-sm-12 col-lg-8 mx-auto py-5 text-center">
+							<p class="mt-3">© 2022 All Rights Reserved | Built by <a href="https://www.instagram.com/phatboitee/"> TeeGee</a></p>
+						</footer>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="https://wizixo.webestica.com/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+	<script src="https://wizixo.webestica.com/assets/vendor/aos/aos.js"></script>
+	<script src="https://wizixo.webestica.com/assets/vendor/glightbox/js/glightbox.js"></script>
+	<script src="https://wizixo.webestica.com/assets/vendor/tiny-slider/tiny-slider.js"></script>
+
+	<script src="https://wizixo.webestica.com/assets/js/functions.js"></script>
+
+</body>
+</html>
